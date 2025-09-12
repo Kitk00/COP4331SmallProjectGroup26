467ecaeb61c9ad5701c5b319f257f7774b42000e
Kit K <147215922+Kitk00@users.noreply.github.com>
2025-09-11 20:56:25 -0400

    testing
    changed url to my test droplet

--- js/code.js
+++ js/code.js
@@ -1,4 +1,5 @@
-const urlBase = 'http://159.89.159.198/LAMPAPI';
+// const urlBase = 'http://159.89.159.198/LAMPAPI';
+const urlBase = 'http://134.209.2.58/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
